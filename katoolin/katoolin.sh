2347a734eee900940268b76291a7a507c19ea57f
zhangjiacheng1991 <437525123@qq.com>
2019-05-04 22:28:31 +0800

    修改katools脚本

--- katoolin/katoolin.sh
+++
@@ -1,8 +0,0 @@
-############################################
-# 文件名: MyData/katoolin/katoolin.sh
-# 作者：jiacheng
-# 邮箱：437525123@qq.com
-# 创建时间：2019年03月26日 星期二 23时07分00秒
-#############################################
-#!/bin/bash
-git clone https://github.com/LionSec/katoolin.git && cp katoolin/katoolin.py     /usr/bin/katoolin && chmod +x /usr/bin/katoolin && apt-get install python-dev
